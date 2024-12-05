bb030809f7aded54f4beaba30fddd7fbf5bee885
LITIE <hnm7877@gmail.com>
2024-12-05 13:07:14 +0100

    fixe bug deploiement

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { db } from "@/db"; // Assurez-vous que le chemin est correct vers votre `db/index.ts`
@@ -32,53 +33,57 @@ export default async function DashboardPage() {
   }
 
   return (
-    <main className="min-h-screen bg-gradient-to-b from-background to-secondary py-20">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="text-center space-y-4 mb-16">
-          <h1 className="text-4xl font-bold">Bienvenue, {user?.given_name}</h1>
-          <p className="text-xl text-muted-foreground">
-            Que souhaitez-vous personnaliser aujourd&apos;hui ?
-          </p>
-        </div>
-
-        <div className="grid md:grid-cols-2 gap-8">
-          {/* Card pour les images */}
-          <div>
-            <Card className="p-6 space-y-4 hover:shadow-lg transition-shadow">
-              <div className="h-12 w-12 rounded-lg bg-primary/10 flex items-center justify-center mb-4">
-                <ImageIcon className="h-6 w-6 text-primary" />
-              </div>
-              <h2 className="text-2xl font-semibold">Images</h2>
-              <p className="text-muted-foreground">
-                Ajoutez des stickers à vos images en quelques clics.
-                Personnalisez la taille et la position selon vos besoins.
-              </p>
-              <br />
-              <Link href="/images" className="block">
-                <Button className="w-full">Commencer avec les images</Button>
-              </Link>
-            </Card>
+    <>
+      <main className="min-h-screen bg-gradient-to-b from-background to-secondary py-20">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="text-center space-y-4 mb-16">
+            <h1 className="text-4xl font-bold">
+              Bienvenue, {user?.given_name}
+            </h1>
+            <p className="text-xl text-muted-foreground">
+              Que souhaitez-vous personnaliser aujourd&apos;hui ?
+            </p>
           </div>
 
-          {/* Card pour les vidéos */}
-          <div>
-            <Card className="p-6 space-y-4 hover:shadow-lg transition-shadow">
-              <div className="h-12 w-12 rounded-lg bg-primary/10 flex items-center justify-center mb-4">
-                <VideoIcon className="h-6 w-6 text-primary" />
-              </div>
-              <h2 className="text-2xl font-semibold">Vidéos</h2>
-              <p className="text-muted-foreground">
-                Donnez vie à vos vidéos avec des stickers animés. Contrôlez leur
-                apparence tout au long de la vidéo.
-              </p>
-              <br />
-              <Link href="/videos" className="block">
-                <Button className="w-full">Commencer avec les vidéos</Button>
-              </Link>
-            </Card>
+          <div className="grid md:grid-cols-2 gap-8">
+            {/* Card pour les images */}
+            <div>
+              <Card className="p-6 space-y-4 hover:shadow-lg transition-shadow">
+                <div className="h-12 w-12 rounded-lg bg-primary/10 flex items-center justify-center mb-4">
+                  <ImageIcon className="h-6 w-6 text-primary" />
+                </div>
+                <h2 className="text-2xl font-semibold">Images</h2>
+                <p className="text-muted-foreground">
+                  Ajoutez des stickers à vos images en quelques clics.
+                  Personnalisez la taille et la position selon vos besoins.
+                </p>
+                <br />
+                <Link href="/images" className="block">
+                  <Button className="w-full">Commencer avec les images</Button>
+                </Link>
+              </Card>
+            </div>
+
+            {/* Card pour les vidéos */}
+            <div>
+              <Card className="p-6 space-y-4 hover:shadow-lg transition-shadow">
+                <div className="h-12 w-12 rounded-lg bg-primary/10 flex items-center justify-center mb-4">
+                  <VideoIcon className="h-6 w-6 text-primary" />
+                </div>
+                <h2 className="text-2xl font-semibold">Vidéos</h2>
+                <p className="text-muted-foreground">
+                  Donnez vie à vos vidéos avec des stickers animés. Contrôlez
+                  leur apparence tout au long de la vidéo.
+                </p>
+                <br />
+                <Link href="/videos" className="block">
+                  <Button className="w-full">Commencer avec les vidéos</Button>
+                </Link>
+              </Card>
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,88 +22,95 @@ export default function Home() {
   }, [isAuthenticated, router]);
 
   return (
-    <div>
-      <section className="relative bg-gradient-to-b from-primary/5 to-background py-20 overflow-hidden">
-        <div className="absolute inset-0 bg-grid-pattern opacity-20" />
-        <div
-          className={cn(
-            "mx-auto w-full max-w-screen-xl px-2.5 md:px-20 mb-12 mt-28 sm:mt-14 flex flex-col items-center justify-center text-center"
-          )}
-        >
-          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 relative">
-            <motion.div
-              className="text-center space-y-6 max-w-3xl mx-auto"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.8 }}
-            >
-              <motion.h1
-                className="text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-primary to-primary/60"
-                initial={{ scale: 0.95 }}
-                animate={{ scale: 1 }}
-                transition={{ delay: 0.2, duration: 0.8, type: "spring" }}
-              >
-                Donnez vie à vos médias avec des stickers animés
-              </motion.h1>
-              <motion.p
-                className="text-lg sm:text-xl text-muted-foreground px-4 sm:px-0"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ delay: 0.4 }}
-              >
-                Une solution simple et rapide pour personnaliser vos images et vidéos. Ajoutez des
-                stickers, exportez et partagez en quelques clics.
-              </motion.p>
+    <>
+      <div>
+        <section className="relative bg-gradient-to-b from-primary/5 to-background py-20 overflow-hidden">
+          <div className="absolute inset-0 bg-grid-pattern opacity-20" />
+          <div
+            className={cn(
+              "mx-auto w-full max-w-screen-xl px-2.5 md:px-20 mb-12 mt-28 sm:mt-14 flex flex-col items-center justify-center text-center"
+            )}
+          >
+            <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 relative">
               <motion.div
-                className="flex flex-col sm:flex-row gap-4 justify-center px-4 sm:px-0"
+                className="text-center space-y-6 max-w-3xl mx-auto"
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.6 }}
+                transition={{ duration: 0.8 }}
               >
-                <Link href="/images" className="w-full sm:w-auto">
-                  <Button size="lg" className="w-full sm:w-auto gap-2">
-                    <ImageIcon className="w-5 h-5" />
-                    Commencer avec les images
-                  </Button>
-                </Link>
-                <Link href="/videos" className="w-full sm:w-auto">
-                  <Button size="lg" variant="outline" className="w-full sm:w-auto gap-2">
-                    <VideoIcon className="w-5 h-5" />
-                    Commencer avec les vidéos
-                  </Button>
-                </Link>
+                <motion.h1
+                  className="text-3xl sm:text-4xl md:text-5xl font-bold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-primary to-primary/60"
+                  initial={{ scale: 0.95 }}
+                  animate={{ scale: 1 }}
+                  transition={{ delay: 0.2, duration: 0.8, type: "spring" }}
+                >
+                  Donnez vie à vos médias avec des stickers animés
+                </motion.h1>
+                <motion.p
+                  className="text-lg sm:text-xl text-muted-foreground px-4 sm:px-0"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 0.4 }}
+                >
+                  Une solution simple et rapide pour personnaliser vos images et
+                  vidéos. Ajoutez des stickers, exportez et partagez en quelques
+                  clics.
+                </motion.p>
+                <motion.div
+                  className="flex flex-col sm:flex-row gap-4 justify-center px-4 sm:px-0"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.6 }}
+                >
+                  <Link href="/images" className="w-full sm:w-auto">
+                    <Button size="lg" className="w-full sm:w-auto gap-2">
+                      <ImageIcon className="w-5 h-5" />
+                      Commencer avec les images
+                    </Button>
+                  </Link>
+                  <Link href="/videos" className="w-full sm:w-auto">
+                    <Button
+                      size="lg"
+                      variant="outline"
+                      className="w-full sm:w-auto gap-2"
+                    >
+                      <VideoIcon className="w-5 h-5" />
+                      Commencer avec les vidéos
+                    </Button>
+                  </Link>
+                </motion.div>
               </motion.div>
-            </motion.div>
 
-            <motion.div
-              className="mt-16 px-4 sm:px-0"
-              initial={{ opacity: 0, y: 40 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ delay: 0.8, duration: 0.8 }}
-            >
-              <div>
-                <div className="relative mx-auto max-w-4xl">
-                  <div className="mt-16 flow-root sm:mt-24">
-                    <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-                      <video
-                        width="1364"
-                        height="866"
-                        autoPlay
-                        className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
-                      >
-                        <source src="/videos/accueil.mp4" type="video/mp4" />
-                      </video>
+              <motion.div
+                className="mt-16 px-4 sm:px-0"
+                initial={{ opacity: 0, y: 40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 0.8, duration: 0.8 }}
+              >
+                <div>
+                  <div className="relative mx-auto max-w-4xl">
+                    <div className="mt-16 flow-root sm:mt-24">
+                      <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                        <video
+                          width="1364"
+                          height="866"
+                          autoPlay
+                          className="rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10"
+                        >
+                          <source src="/videos/accueil.mp4" type="video/mp4" />
+                        </video>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </motion.div>
+              </motion.div>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <FeaturesSection />
-      <DemoSection />
-    </div>
+        <FeaturesSection />
+        <DemoSection />
+      </div>
+    </>
   );
 }
